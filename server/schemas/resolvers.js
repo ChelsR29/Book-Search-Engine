7360378771b81c9b6453d58567e814f6e340984d
Chelsea <chibby@chelseas-mbp.mynetworksettings.com>
2024-06-03 13:04:37 -0400

    Add Resolvers.JS Code

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,63 @@
-const { Tech, Matchup } = require('../models');
+const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        return User.findOne({ _id: context.user._id }).populate('savedBooks');
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
 
+  Mutation: {
+    addUser: async (parent, { username, email, password }) => {
+      const user = await User.create({ username, email, password });
+      const token = signToken(user);
+      return { token, user };
+    },
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw new AuthenticationError('Incorrect credentials');
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+    saveBook: async (parent, { book }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          { $addToSet: { savedBooks: book } },
+          { new: true, runValidators: true }
+        ).populate('savedBooks');
+
+        return updatedUser;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+    removeBook: async (parent, { bookId }, context) => {
+      if (context.user) {
+        const updatedUser = await User.findByIdAndUpdate(
+          context.user._id,
+          { $pull: { savedBooks: { bookId } } },
+          { new: true }
+        ).populate('savedBooks');
+
+        return updatedUser;
+      }
+      throw new AuthenticationError('Not logged in');
+    },
+  },
 };
 
 module.exports = resolvers;
